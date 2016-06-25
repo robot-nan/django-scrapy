c258c401ae015d0e726ea700b76eac65f6d5b7c1
Robot <a.robot.n@gmail.com>
2016-06-26 01:57:24 +0800

    update

--- django_scrapy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-WORKING_DIR=/Users/robot/django-scrapy
-ACTIVATE_PATH=~/.virtualenvs/django-scrapy/activate
-cd ${WORKING_DIR}
-source ${ACTIVATE_PATH}
-exec $@
--- super_worm/urls.py
+++ super_worm/urls.py
@@ -18,5 +18,4 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^admin123/', admin.site.urls),
 ]
