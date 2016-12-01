f93521fc7956c920b88e9f08ba47d3529895350b
Robot <a.robot.n@gmail.com>
2016-12-01 22:46:23 +0800

    no message

--- finance/models.py
+++ finance/models.py
@@ -8,3 +8,15 @@ class DayBuyPoint(BaseTime):
 
     def __unicode__(self):
         return self.point
+
+
+class StockPoint(BaseTime):
+    date = models.CharField(u'日期', max_length=16)
+    code = models.CharField(u'股票代码', max_length=16)
+    price = models.CharField(u'价格', max_length=16)
+    time = models.CharField(u'时间', max_length=32)
+    type = models.CharField(u'类型', max_length=4)
+
+    def __unicode__(self):
+        return self.code
+
