56af3c0f855a90476fd6513215e1147a268f2f04
Robot <a.robot.n@gmail.com>
2017-01-22 22:53:45 +0800

    update

--- super_worm/settings.py
+++ super_worm/settings.py
@@ -21,6 +21,8 @@ codecs.register(lambda name: codecs.lookup('utf8') if name == 'utf8mb4' else Non
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '94(%%3un8vtf3=u-b3gm%0r!!h3(j#e01rfmo#b$+=bg6w3o#('
+SHUJUBAO_GOLD_KEY = 'ce42c2c5418a5cb266657b25f9067965'
+SHUJUBAO_SILVER_KEY = '12916b979f23b5f51b997c9f1d0c1da4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
